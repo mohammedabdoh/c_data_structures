86f9f1430d44a5fe21b25cac1ee127ac66f8541b
Mohammed Abdoh <firefoxegy@gmail.com>
2021-10-03 18:45:30 +0200

    Excercising functions in c

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int add (int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    printf("%d\n", add(2, 3));
+    return 0;
+}
+
+
---
+++ pass_by_reference.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void swap(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+
+
+int main()
+{
+    int a, b;
+    
+    a = 10;
+    b = 20;
+
+    // Passing by value
+    swap(a, b);
+    printf("a: %d, b: %d\n", a, b);
+
+    return 0;
+}
+
+
