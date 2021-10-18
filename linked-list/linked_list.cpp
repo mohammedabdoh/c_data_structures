54f1028deafc47dd00a5edc6e7377bf842b34810
Mohammed Abdoh <firefoxegy@gmail.com>
2021-10-18 20:31:39 +0200

    Added a template to the Node class

--- linked-list/linked_list.cpp
+++ linked-list/linked_list.cpp
@@ -1,60 +1,64 @@
 #include <iostream>
 
+using std::cout;
+
+template<class T>
 class Node
 {
 public:
-    int data;
+    T data;
     Node *nextNode;
         
-    explicit Node(int value) {
+    explicit Node(T value) {
         data = value;
         nextNode = nullptr;
     }
 };
 
+template<class T>
 class LinkedList
 {
 public:
-    Node *head;
+    Node<T> *head;
         
     LinkedList()
     {
         head = nullptr;
     }
         
-    void prepend(Node *nodeToInsert)
+    void prepend(Node<T> *nodeToInsert)
     {
         nodeToInsert->nextNode = this->head;
         this->head = nodeToInsert;
     }
         
-    void append(Node *nodeToInsert) const
+    void append(Node<T> *nodeToInsert) const
     {
-        Node *currentNode = this->head;
+        Node<T> *currentNode = this->head;
         while(currentNode->nextNode != nullptr) {
             currentNode = currentNode->nextNode;
         }
         currentNode->nextNode = nodeToInsert;
     }
         
-    static void insertAfterNode(Node *nodeToInsertAfter, Node *nodeToInsert)
+    static void insertAfterNode(Node<T> *nodeToInsertAfter, Node<T> *nodeToInsert)
     {
         nodeToInsert->nextNode = nodeToInsertAfter->nextNode;
         nodeToInsertAfter->nextNode = nodeToInsert;
     }
         
-    void deleteNode(Node *nodeToDelete) const
+    void deleteNode(Node<T> *nodeToDelete) const
     {
-        Node *currentNode = this->head;
+        Node<T> *currentNode = this->head;
         while(currentNode->nextNode != nodeToDelete) {
             currentNode = currentNode->nextNode;
         }
         currentNode->nextNode = nodeToDelete->nextNode;
     }
         
-    Node *findNode(int value) const
+    Node<T> *findNode(T value) const
     {
-        Node *currentNode = this->head;
+        Node<T> *currentNode = this->head;
         while(currentNode->nextNode != nullptr) {
             if(currentNode->data == value) {
                 return currentNode;
@@ -66,28 +70,45 @@ public:
         
     void print() const
     {
-        Node *currentNode = this->head;
-        printf("head -> ");
+        Node<T> *currentNode = this->head;
+        cout << "head -> ";
         while(currentNode != nullptr) {
-            printf("%d -> ", currentNode->data);
+            cout << currentNode->data << " -> ";
             currentNode = currentNode->nextNode;
         }
-        printf("NULL\n");
+        cout << "NULL\n";
     }
 };
 
 int main() {
-    LinkedList linkedList;
+
+    // Linked list of ints
+    LinkedList<int> linkedList;
     
     for ( int i = 0; i < 5; i++ ) {
-        linkedList.prepend(new Node(i+1));
+        linkedList.prepend(new Node<int>(i+1));
     }
     
-    linkedList.append(new Node(100));
-    LinkedList::insertAfterNode(linkedList.findNode(3), new Node(102));
+    linkedList.append(new Node<int>(100));
+    LinkedList<int>::insertAfterNode(linkedList.findNode(3), new Node<int>(102));
     linkedList.deleteNode(linkedList.findNode(1));
     
     linkedList.print();
+
+
+    // Linked list of chars
+    LinkedList<char> charLinkedList;
+
+    charLinkedList.prepend(new Node<char>('a'));
+    charLinkedList.prepend(new Node<char>('b'));
+    charLinkedList.prepend(new Node<char>('c'));
+    charLinkedList.prepend(new Node<char>('d'));
+
+    charLinkedList.append(new Node<char>('e'));
+    LinkedList<char>::insertAfterNode(charLinkedList.findNode('b'), new Node<char>('f'));
+    charLinkedList.deleteNode(charLinkedList.findNode('a'));
+
+    charLinkedList.print();
     
     return 0;
 }
