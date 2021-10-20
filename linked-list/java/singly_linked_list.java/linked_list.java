71a233efefd6e895f2f67d565059ade99918ef35
Mohammed Abdoh <firefoxegy@gmail.com>
2021-10-21 01:04:01 +0200

    Implemented Doubly Linked List DLL in Java

--- linked-list/java/singly_linked_list.java/linked_list.java
+++ linked-list/java/singly_linked_list.java/linked_list.java
@@ -1,14 +1,3 @@
-
-class Node<T>
-{
-    T data;
-    Node<T> nextNode;
-    
-    Node(T value) {
-        data = value;
-    }
-}
-
 class LinkedList<T>
 {
     Node<T> head;
---
+++ linked-list/java/singly_linked_list.java/node.java
@@ -0,0 +1,9 @@
+class Node<T>
+{
+    T data;
+    Node<T> nextNode;
+    
+    Node(T value) {
+        data = value;
+    }
+}
