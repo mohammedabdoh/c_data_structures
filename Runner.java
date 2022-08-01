93d12570f559bd295a6273a61f9badac5c2aeb34
Mohammed Abdoh <36000898+mohammedabdoh@users.noreply.github.com>
2022-08-01 12:11:39 +0000

    Created a java runner class

---
+++ Runner.java
@@ -0,0 +1,11 @@
+import linked_list.java.doubly_linked_list.DoublyLinkedListMain;
+import linked_list.java.doubly_linked_list_with_tail.DoublyLinkedListWithTailMain;
+import linked_list.java.singly_linked_list.SinglyLinkedListMain;
+
+public class Runner {
+    public static void main(String[] args) {
+        SinglyLinkedListMain.run();
+        DoublyLinkedListWithTailMain.run();
+        DoublyLinkedListMain.run();
+    }
+}
